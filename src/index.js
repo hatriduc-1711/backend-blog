96deebfdf91e218ebf66e259c3932ec34e4681ea
Ha Tri Duc <hatriduc32@gmail.com>
2023-02-23 02:47:52 +0700

    aa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import { viewEngine } from './config/viewEngine.js';
@@ -8,6 +9,8 @@ import { connectDB } from './config/connectDB.js';
 dotenv.config();
 const port = process.env.PORT || 9090;
 const app = express();
+
+mongoose.set('strictQuery', true);
 
 app.use(cors({ origin: process.env.REACT_URL }));
 app.use(bodyParser.urlencoded({ extended: false }));
